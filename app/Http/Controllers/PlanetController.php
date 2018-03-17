985a355bcf39dc0ff7d20f421e8e22270e3f368b
brytorres <bryan@torvel.us>
2018-03-17 15:35:46 -0400

    Create Planets Retrograde Data API

---
+++ app/Http/Controllers/PlanetController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Planet;
+use App\Http\Resources\Planet as PlanetResource;
+
+class PlanetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // Get Planets data
+        $planets = Planet::paginate(1);
+
+        // Return collection as a resource   
+        return PlanetResource::collection($planets);
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // Get Planet date data
+        $planet = Planet::findOrFail($id);
+        
+        return new PlanetResource($planet);
+    }
+
+    
+}
